8e3bb5432182431190a7550d3ef71f9296c0b750
Mathias Ziegler <moredhel@moredhel.de>
2020-11-21 17:19:32 +0100

    Update read-hum-press-temp.sh
    read temperture sensor

--- read-hum-press-temp.sh
+++ read-hum-press-temp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-i2cdump -y -r 0xf7-0xfe 2 0x77 > /var/measurement/temperature
+/usr/sbin/i2cdump -y -r 0xf7-0xfe 2 0x77 > /var/measurement/temperature
 chmod a+r /var/measurement/temperature
