33d3e961ec3a6f6403993056e4e2e9b355719ab4
Mathias Ziegler <moredhel@moredhel.de>
2020-11-21 17:20:28 +0100

    Update read-trim.sh
    read trimming values from chip

--- read-trim.sh
+++ read-trim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-i2cdump -y -r 0x88-0xa1 2 0x77 > /var/measurement/trimming
-i2cdump -y -r 0xe1-0xe6 2 0x77 >> /var/measurement/trimming
+/usr/sbin/i2cdump -y -r 0x88-0xa1 2 0x77 > /var/measurement/trimming
+/usr/sbin/i2cdump -y -r 0xe1-0xe6 2 0x77 >> /var/measurement/trimming
 chmod a+r /var/measurement/trimming
