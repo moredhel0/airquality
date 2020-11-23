1ef3e1a898f4a092e47f871b238cd82173421573
Mathias Ziegler <moredhel@moredhel.de>
2020-11-23 15:30:29 +0100

    read trimming values

--- read-trim.sh
+++ read-trim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /usr/sbin/i2cdump -y -r 0x88-0xa1 2 0x77 > /var/measurement/trimming
-/usr/sbin/i2cdump -y -r 0xe1-0xe6 2 0x77 >> /var/measurement/trimming
+/usr/sbin/i2cdump -y -r 0xe1-0xe7 2 0x77 >> /var/measurement/trimming
 chmod a+r /var/measurement/trimming
