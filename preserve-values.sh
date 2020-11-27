9f335f86c43636475b4d2145bf01fea696efdf1c
Mathias Ziegler <moredhel@moredhel.de>
2020-11-27 22:41:18 +0100

    Delete preserve-values.sh

--- preserve-values.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for i in {99..0}
-    do 
-	num=$((1 +$i))
-	cp /var/measurement/CO2-sicher$i /var/measurement/CO2-sicher$num
-	cp /var/measurement/temperature-sicher$i /var/measurement/temperature-sicher$num
-    done
-cp /var/measurement/CO2-data /var/measurement/CO2-sicher0
-cp /var/measurement/temperature /var/measurement/temperature-sicher0
